ab19ef89683d12e7348f94a0eefe3c35710413c4
vinay-117 <vinaymaddimsetti114@gmail.com>
2023-11-25 12:18:37 +0530

    Updated commit

--- force-app/main/default/lwc/lifecycleHooks/lifecycleHooks.js
+++ force-app/main/default/lwc/lifecycleHooks/lifecycleHooks.js
@@ -1,9 +1,27 @@
 import { LightningElement } from 'lwc';
 
 export default class LifecycleHooks extends LightningElement {
-
+    myList = [];
     constructor(){
         super();
-        console.log('Constructor');
+        console.log("Constructor");
+    }
+
+    connectedCallback(){
+        this.myList.push("Salesforce LWC");
+        console.log("ConectedCallBack");
+    }
+
+    disconnectedCallback(){
+        this.myList = [];
+        console.log("disconnectedCallBack");
+    }
+
+    renderedCallback(){
+        console.log("renderCallBack");
+    }
+
+    errorCallback(error, stack){
+        console.log('Error call back: ' + error);
     }
 }
